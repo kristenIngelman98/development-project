58f272983cbafd60e2af32344972478ccf22e1c0
Kristen <kristeningelman@hotmail.com>
2023-01-12 08:25:09 -0800

    created basic todos and todo components

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+    setTodos([prevTodos => {
+      return [...prevTodos, { id: 1, name: name, complete: false }]
+    }])
+    todoNameRef.current.value = null
+  }
+
+  return (
+    <>
+     <TodoList todos={todos}/>
+     <input ref={todoNameRef} type="text"></input>
+     <button onClick={handleAddTodo}>Add Todo</button>
+     <button>Clear Completed Todos</button>
+     <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
