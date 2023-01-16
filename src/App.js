387d691a68312cda54c1c09c7ed568797f949074
Kristen <kristeningelman@hotmail.com>
2023-01-16 11:33:33 -0800

    saved todos to localStorage

--- src/App.js
+++ src/App.js
@@ -3,47 +3,30 @@ import TodoList from './TodoList';
 import uuid from 'react-uuid'
 
 
-const LOCAL_STORAGE_KEY = `todoApp.todos`
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  // const initialCount = 0
-  // const [count, setCount] = useState(initialCount)
+  const [todos, setTodos] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY)
+    const initialValue = JSON.parse(saved)
+    return initialValue || []
+  });
 
-  // const incrementFive = () => {
-  //   for(let i = 0; i < 5; i++) {
-  //     setCount(prevCount => prevCount + 1)
-  //   }
-  // }
-
-  // return (
-  //   <div>
-  //     Count: {count}
-  //     <button onClick={() => setCount(initialCount)}>Reset</button>
-  //     <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
-  //     <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
-  //     <button onClick={incrementFive}>Increment by 5</button>
-  //   </div>
-  // )
-  const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
-  // use the second useEffect to load to todos saved in local storage
-  useEffect(() => {
-    console.log('in second effect')
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    console.log('stored todos', storedTodos)
-    if(storedTodos) {
-      setTodos(storedTodos)
-    }
-    console.log('after if', storedTodos)
-  }, []) // use empty array so that this function only runs once, right at the beginning (to load the items in local storage)
-
-  // first useEffect saves all todos to local storage
+  // useEffect saves all todos to local storage
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
     console.log(localStorage)
   }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos] // should never directly modify a state variable, always create a copy before modifying (use copy to modify state)
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -53,13 +36,19 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+
+  }
+
   return (
     <>
-     <TodoList todos={todos}/>
+     <TodoList todos={todos} toggleTodo={toggleTodo} />
      <input ref={todoNameRef} type="text"></input>
      <button onClick={handleAddTodo}>Add Todo</button>
-     <button>Clear Completed Todos</button>
-     <div>0 left to do</div>
+     <button onClick={handleClearTodos}>Clear Completed Todos</button>
+     <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
